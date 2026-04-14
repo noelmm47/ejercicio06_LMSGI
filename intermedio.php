2fee9747183947bc2bf4d15dc70ae824b998cf7c
noelmm47 <noelmm47@educastur.es>
2026-04-14 18:12:58 +0200

    Mejorando intermedio.php
    Al final, Víctor y yo estuvimos la anterior clase haciendo lo mismo sin darnos cuenta. El que hizo él estaba mejor planteado, así que nos quedaremos con esta versión (a la que le añadí lo de la sesión).

--- intermedio.php
+++ intermedio.php
@@ -6,21 +6,33 @@
 		header($_POST['Dir']);
 ?>
 
-<!DOCTYPE HTML>
-<lang="es">
-<head> 
-	<meta charset="utf-8">
-	<title></title>
-</head>
+<!DOCTYPE html>
+<html lang="es">
 
-<body> 
-	<form action="intermedio.php" method="POST">
-		<input type="radio" name="Dir" value="clientes.php"/><label>Clientes</label>
-		<input type="radio" name="Dir" value="items.php"/><label>Libros</label>
-		<input type="radio" name="Dir" value="items.php"/><label>Películas</label>
-		<!-- Hacer que de alguna forma se sepa si se quieren libros o películas -->
-		<input type="submit" value="Seleccionar"/>
-	</form>
-</body>
+	<head>
+	<meta charset="UTF-8">
+	</head>
+	
+	<body>
+		<h1>¿Qué deseas gestionar?</h1>
+		<br>
+		<h3>Clientes:</h3>
+		<br>
+		<p>Agrega nuevos clientes a la base de datos, modifica o borra los ya existentes.</p>
+		<form action="clientes.php" method="POST">
+			<button type="submit">Acceder</button>
+		</form>
+		<br>
+		
+		<h3>Catálogo:</h3>
+		<br>
+		<p>Gestiona la reserva y préstamo de libros y películas, agrega nuevos títulos al catálogo.</p>
+		<form action="productos.php" method="POST">
+		<button type="submit" name="acceso" value="libros">Libros</button>
+		<br>
+		<br>
+		<button type="submit" name="acceso" value="peliculas">Películas</button>
+		</form>
+	</body>
 
 </html>
