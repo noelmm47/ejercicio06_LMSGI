7351a00753d34203ebc394cda69d52fab521beab
noelmm47 <noelmm47@educastur.es>
2026-04-15 18:39:42 +0200

    Conexión a la BBDD actualizada
    Ahora la conexión con la Base de Datos está codificada en UTF-8 y no nos dará problemas con tildes ni ñ.
    
    Gracias, Gonzalo.

--- conexion_bbdd.php
+++ conexion_bbdd.php
@@ -6,6 +6,7 @@
     $nombre_bbdd = "proyecto";
 
     $conexion = new mysqli($servidor, $usuario, $contraseña, $nombre_bbdd);
+	$conexion->set_charset("utf8");
 
     if($conexion->connect_error) {
         echo "Hubo un error" . $conexion->connect_error;
